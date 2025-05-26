080e0f49a897b458f5008865dd2ee819175631b0
isabela557 <isabela.chetran@edu.uah.es>
2025-05-26 12:28:44 +0200

    Main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -10,19 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        try {
-            PantallaManager.setStage(stage); // Asigna el Stage global
-
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/PantallaPrincipal.fxml"));
-            Scene scene = new Scene(loader.load(), 800, 500);
-
-            stage.setTitle("Juego de Conquista");
-            stage.setScene(scene);
-            stage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        PantallaManager.setStage(stage);
+        PantallaManager.mostrarPantallaPrincipal();
     }
 
     public static void main(String[] args) {
